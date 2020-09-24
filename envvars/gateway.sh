fe15b68dad97b7a1b73c1f5342cf40baa8788aa5
Prakhar Srivastava <31232641+prakhar1985@users.noreply.github.com>
2020-09-24 16:44:42 +1000

    Update gateway.sh

--- envvars/gateway.sh
+++ envvars/gateway.sh
@@ -17,9 +17,9 @@
 #export DEFAULT_PAGE='workshop'
 
 # Flags to explicitly enable/disable specific components
-#ENABLE_CONSOLE='false'
+ENABLE_CONSOLE='false'
 #ENABLE_DASHBOARD='false'
 #ENABLE_SLIDES='false'
-#ENABLE_TERMINAL='false'
+ENABLE_TERMINAL='false'
 #ENABLE_WEBDAV='false'
 #ENABLE_WORKSHOP='true'
